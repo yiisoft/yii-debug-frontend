91e2e8febcbb681586b9d3a84dd7fb56ce590b7e
Julian Böhnke <info@julianb.de>
2021-01-07 22:27:22 +0100

    update core and cli to version 11

--- src/app/pages/view/layout/view-layout.component.spec.ts
+++ src/app/pages/view/layout/view-layout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ViewLayoutComponent } from './view-layout.component';
 
@@ -6,7 +6,7 @@ describe('ViewLayoutComponent', () => {
   let component: ViewLayoutComponent;
   let fixture: ComponentFixture<ViewLayoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ViewLayoutComponent],
     })
