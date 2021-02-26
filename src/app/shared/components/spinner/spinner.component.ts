1bc88f5cd2d69b3469cd6dc884a47dd8e7bc1ed8
Rustam Mamadaminov <rmamdaminov@gmail.com>
2021-02-26 18:37:54 +0500

    Add new spinner, improve gitignore

---
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'yii-spinner',
+    templateUrl: './spinner.component.html',
+    styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent {}
